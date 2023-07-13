6bc7c197c862e11bde0b3509527f83e67f9535bc
Ema <ema.smolic@fer.hr>
2023-07-13 07:12:34 +0200

    .

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.listen(3000, () => {
 	const wss = new WebSocket.Server({ port: 4000 })
 wss.on('connection', ws => {
   ws.on('message', message => {
-    console.log(`Received message => ${message}`)
+    ws.send(message)
   })
-  ws.send('Hello! Message From Server!!')
+
 })
